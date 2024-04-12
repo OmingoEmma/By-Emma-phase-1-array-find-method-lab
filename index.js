6c21ca1ae963fc8b295b8ebee27fa3a4a854bd6c
OmingoEmma <emmaomingo@gmail.com>
2024-04-12 03:36:08 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+const superbowlWin = (record) => {
+    
+    const wins = record.filter(entry => entry.result === 'W');
+  
+    
+    if (wins.length === 0) {
+      return undefined;
+    }
+  
+    
+
+    return wins[0].year;
+  }
+  
